a67e5274373665e4493f8b5c73a15a802b60ecbf
JothLee <550255219@qq.com>
2019-05-28 19:13:45 +0800

    简单登录注册界面

---
+++ app/src/main/java/com/example/notes/notetaking/Activity/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.notes.notetaking.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.notes.notetaking.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
