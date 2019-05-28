069cd0e5148f61edbcc37bc06267844f9e8657c2
Momoko_W <1165874815@qq.com>
2019-05-28 08:53:11 +0800

    本次此修改创建了底部导航栏，实现简单的页面

--- app/src/main/java/com/example/notes/notetaking/Activity/MainActivity.java
+++ app/src/main/java/com/example/notes/notetaking/Activity/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.notes.notetaking.Activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.notes.notetaking.R;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text);
+        navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        //选中条目的监听事件
+        navigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        textView.setText(menuItem.getTitle().toString());
+                        return true;
+                    }
+                }
+        );
+
     }
 }
