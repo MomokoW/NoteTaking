078191c19cc050cb5bd4a5477847a8ba49572d51
JothLee <550255219@qq.com>
2019-05-29 09:55:15 +0800

    “我的”界面

--- app/src/main/java/com/example/notes/notetaking/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/notes/notetaking/Activity/RegisterActivity.java
@@ -15,8 +15,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
     }
