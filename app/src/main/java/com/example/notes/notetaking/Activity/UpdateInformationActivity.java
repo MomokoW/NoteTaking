493c390c32b027ab9aad0c4fd6a7b6e509aeaedf
JothLee <550255219@qq.com>
2019-05-30 09:17:36 +0800

    MainUser类

--- app/src/main/java/com/example/notes/notetaking/Activity/UpdateInformationActivity.java
+++ app/src/main/java/com/example/notes/notetaking/Activity/UpdateInformationActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.notes.notetaking.R;
 
@@ -11,11 +12,19 @@ public class UpdateInformationActivity extends AppCompatActivity {
 
     private Button headChangeBtn;
     private Button inforChangeBtn;
+    private EditText nameChange;
+    private  EditText passwordOld;
+    private EditText passwordChange1;
+    private EditText passwordChange2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_information);
         headChangeBtn = (Button) findViewById(R.id.headChangeButton);
+        nameChange = (EditText)findViewById(R.id.nameChangeEdit);
+        passwordOld = (EditText)findViewById(R.id.passwordOldEdit);
+        passwordChange1 = (EditText)findViewById(R.id.passwordNew1Edit);
+        passwordChange2 = (EditText)findViewById(R.id.passwordNew2Edit);
         //选择图片，修改头像
         headChangeBtn.setOnClickListener(new View.OnClickListener(){
 
@@ -29,6 +38,10 @@ public class UpdateInformationActivity extends AppCompatActivity {
         inforChangeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String nameNew = nameChange.getText().toString();
+                String passOld = passwordOld.getText().toString();
+                String passNew1 = passwordChange1.getText().toString();
+                String passNew2 = passwordChange2.getText().toString();
 
             }
         });
