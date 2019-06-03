f63f2e9c4ce741aa012cf7df323962155953464d
JothLee <550255219@qq.com>
2019-06-03 22:35:03 +0800

    完成update功能，修改setting，

--- app/src/main/java/com/example/notes/notetaking/Fragment/Settings.java
+++ app/src/main/java/com/example/notes/notetaking/Fragment/Settings.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -31,6 +32,7 @@ import com.example.notes.notetaking.Model.MainUser;
 import com.example.notes.notetaking.R;
 import com.makeramen.roundedimageview.RoundedImageView;
 
+import java.io.File;
 import java.net.URL;
 
 /**
@@ -50,6 +52,7 @@ public class Settings extends Fragment {
     private AlertDialog.Builder builder = null;
     private int photoChoice;
     private Bitmap bitmap;
+    private Uri imageUrl;
     public Settings() {
         // Required empty public constructor
     }
@@ -140,9 +143,12 @@ public class Settings extends Fragment {
 
     /*调用相机*/
     private void goXiangJi() {
+
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,false);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,"data");
         startActivityForResult(intent, 1);
+
+       // File outputImage = new File(getExternalCacheDir(),"output_image.jpg");
     }
 
     /*调用相册*/
@@ -151,6 +157,47 @@ public class Settings extends Fragment {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         intent.setType("image/*");
         startActivityForResult(intent, 111);
+    }
+    //修改图片
+    public Bitmap changePhoto(Bitmap bitmap,int edgeLength){
+        if(null == bitmap || edgeLength <= 0)
+        {
+            return  null;
+        }
+
+        Bitmap result = bitmap;
+        int widthOrg = bitmap.getWidth();
+        int heightOrg = bitmap.getHeight();
+
+        if(widthOrg > edgeLength && heightOrg > edgeLength)
+        {
+            //压缩到一个最小长度是edgeLength的bitmap
+            int longerEdge = (int)(edgeLength * Math.max(widthOrg, heightOrg) / Math.min(widthOrg, heightOrg));
+            int scaledWidth = widthOrg > heightOrg ? longerEdge : edgeLength;
+            int scaledHeight = widthOrg > heightOrg ? edgeLength : longerEdge;
+            Bitmap scaledBitmap;
+
+            try{
+                scaledBitmap = Bitmap.createScaledBitmap(bitmap, scaledWidth, scaledHeight, true);
+            }
+            catch(Exception e){
+                return null;
+            }
+
+            //从图中截取正中间的正方形部分。
+            int xTopLeft = (scaledWidth - edgeLength) / 2;
+            int yTopLeft = (scaledHeight - edgeLength) / 2;
+
+            try{
+                result = Bitmap.createBitmap(scaledBitmap, xTopLeft, yTopLeft, edgeLength, edgeLength);
+                scaledBitmap.recycle();
+            }
+            catch(Exception e){
+                return null;
+            }
+        }
+
+        return result;
     }
 
     //得到结果
@@ -161,14 +208,18 @@ public class Settings extends Fragment {
             ContentResolver cr = getActivity().getContentResolver();
             try {
                 if (photoChoice==0){
+                    //从相册获得图片
                     bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
                 }else {
-                    bitmap = (Bitmap) data.getExtras().get("data");
+                    //从相机获得图片
+                    //Bundle bundle = data.getExtras();//获取封装好传递过来的数据，整个图片的二进制流
+                    bitmap = data.getParcelableExtra("data");
+                    //bitmap = (Bitmap) data.getExtras().get("data");
                 }
                 if (uri ==null){
                     uri = Uri.parse(MediaStore.Images.Media.insertImage(getActivity().getContentResolver(), bitmap, null,null));
                 }
-                photoHead.setImageBitmap(bitmap);
+                photoHead.setImageBitmap(changePhoto(bitmap,80));
             } catch (Exception e) {
                 e.printStackTrace();
             }
